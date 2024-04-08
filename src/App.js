8f7d0e6eb342b7d8156ca29949efb0483d387c27
arpee <richard@saito.tech>
2024-04-08 20:58:32 +1000

    added datestamp

--- src/App.js
+++ src/App.js
@@ -80,6 +80,20 @@ function App() {
     set_burger(false);
   }
 
+  const getFormattedDateTime = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Month from 01 to 12
+    const day = now.getDate().toString().padStart(2, '0'); // Day of the month from 01 to 31
+    const hours = now.getHours().toString(); // Hours in 24-hour format without leading zero
+    const minutes = now.getMinutes().toString(); // Minutes without leading zero
+  
+    // Format: YYYY-MM-DD H:M
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  }
+
+  const daterun = getFormattedDateTime();
+
   return (
     <div className="App">
 
@@ -92,6 +106,7 @@ function App() {
       <header>
         <div className="center_header">
           <h1>SaitoTicker 🚀 <span>{average_price && average_price.toFixed(4)+'$'}</span></h1>
+          <h4>[ {daterun} ]</h4>
           <nav id="nav_id">
             <ul>
               <li onClick={() => close_nav()}>
